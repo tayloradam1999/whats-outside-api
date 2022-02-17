bf575802e888f8be8a6de7459454770dd75d65ac
Adam Taylor <tayloradam1999@gmail.com>
2022-02-16 20:09:13 -0600

    Update SQL Documentation

--- scripts/setupDatabase.sql
+++ scripts/setupDatabase.sql
@@ -4,6 +4,9 @@
 --
 -- After your postgres database is created through installing PostgreSQL,
 -- run this script to setup the database in the format of my UML database documentation.
+--
+-- Since this application will only be ran locally, there is no need to create a new user
+-- for the database, so the user is set to postgres.
 
 -- Create table 'cities'
 CREATE TABLE IF NOT EXISTS cities(
